44b774e86bbba7e4f172ebfd66057a02c839c3a1
User <User@Mohammed-Alaanzy>
2026-06-21 13:43:20 +0500

    with all results

--- examples/org/workflowsim/examples/planning/LIWSABenchmarkExample.java
+++ examples/org/workflowsim/examples/planning/LIWSABenchmarkExample.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012-2013 University Of Southern California
+ * Copyright 2026-2027 SDU University, Kazakhstan
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
